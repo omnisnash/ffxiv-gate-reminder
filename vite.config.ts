46b65355820d478818544e7ca5ed4a035af997e0
Clément Garin <clement.garin@outlook.fr>
2024-06-22 15:18:00 +0200

    Add root base

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@/assets": path.resolve(__dirname, "./src/assets"),
-      "@/components": path.resolve(__dirname, "./src/components"),
+    plugins: [react()],
+    base: '/ffxiv-gate-reminder',
+    resolve: {
+        alias: {
+            '@/assets': path.resolve(__dirname, './src/assets'),
+            '@/components': path.resolve(__dirname, './src/components'),
+        },
     },
-  },
 });
